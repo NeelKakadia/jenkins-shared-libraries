d78bcdbec7999e9a276e1add100a2e131210447b
Neel Kakadia <neelkakadia@Neels-MacBook-Air.local>
2025-05-22 00:21:40 -0400

    dockerhub push lib modified

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -1,6 +1,6 @@
 def call(String Project, String ImageTag, String dockerhubuser){
-  withCredentials([usernamePassword(credentialsId: 'docker', passwordVariable: 'dockerhubpass', usernameVariable: 'dockerhubuser')]) {
-      sh "docker login -u ${dockerhubuser} -p ${dockerhubpass}"
+  withCredentials([usernamePassword(credentialsId: 'docker', passwordVariable: 'DOCKER_PASS', usernameVariable: 'DOCKER_USER')]) {
+      sh "echo "$DOCKER_PASS" | docker login -u "$DOCKER_USER" --password-stdin"
   }
   sh "docker push ${dockerhubuser}/${Project}:${ImageTag}"
 }
