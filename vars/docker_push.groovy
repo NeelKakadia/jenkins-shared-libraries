0a63e8824ef21d16bd675fe781ba220dea85d3d9
Neel Kakadia <neelkakadia@Neels-MacBook-Air.local>
2025-05-22 00:27:55 -0400

    Dock push modified

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -1,6 +1,8 @@
 def call(String Project, String ImageTag, String dockerhubuser){
   withCredentials([usernamePassword(credentialsId: 'docker', passwordVariable: 'DOCKER_PASS', usernameVariable: 'DOCKER_USER')]) {
-      sh "echo "$DOCKER_PASS" | docker login -u "$DOCKER_USER" --password-stdin"
+      sh '''
+      echo "$DOCKER_PASS" | docker login -u "$DOCKER_USER" --password-stdin
+      '''
   }
   sh "docker push ${dockerhubuser}/${Project}:${ImageTag}"
 }
